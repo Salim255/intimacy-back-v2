61e94859d66644dba02d397d8a2e1b1ccf2c44e1
Salim <salim.hassan.welleat@gmail.com>
2025-04-14 16:04:45 +0200

    built: test create user keys

--- src/modules/user-keys/controllers/user-keys.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-
-@ApiTags('user-keys')
-@Controller('user-keys')
-export class UserKeysController {}
--- src/modules/user-keys/user-keys.module.ts
+++ src/modules/user-keys/user-keys.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserKeysService } from './services/user-keys.service';
-import { UserKeysController } from './controllers/user-keys.controller';
 import { UserKeysRepository } from './repository/user-keys.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserKeys } from './entities/user-keys.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserKeys])],
-  controllers: [UserKeysController],
   providers: [UserKeysRepository, UserKeysService],
   exports: [UserKeysService],
 })
