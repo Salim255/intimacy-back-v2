4623ee7fe462ac05b38685bba8e0949d92b3a8dc
Salim <salim.hassan.welleat@gmail.com>
2025-06-04 10:06:48 +0200

    add: debug section

--- test/context.ts
+++ test/context.ts
@@ -22,6 +22,7 @@ export class TestContext {
   private connectionString: string;
   constructor(roleName: string) {
     this.roleName = roleName;
+    console.log('DB_TEST_PASSWORD:', typeof process.env.DB_TEST_PASSWORD);
     this.connectionString = `postgresql://${roleName}:${roleName}@${DEFAULT_OPTS.host}:${DEFAULT_OPTS.port}/${DEFAULT_OPTS.database}`;
   }
 
